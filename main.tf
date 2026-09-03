1923c325ff0b08366a25a6d3edad0dd35873f8fa
Daniel Allen <dan@aeternagroup.net>
2026-09-03 11:40:56 +0100

    Update to schema version v5.0.1

--- main.tf
+++ main.tf
@@ -50,36 +50,36 @@ locals {
 }
 
 module "subscriptions" {
-  source        = "git::https://github.com/AeternaModules/azurerm_subscription.git?ref=v5.0.0"
+  source        = "git::https://github.com/AeternaModules/azurerm_subscription.git?ref=v5.0.1"
   subscriptions = local.subscriptions
 }
 
 module "subscription_cost_management_exports" {
-  source                               = "git::https://github.com/AeternaModules/azurerm_subscription_cost_management_export.git?ref=v5.0.0"
+  source                               = "git::https://github.com/AeternaModules/azurerm_subscription_cost_management_export.git?ref=v5.0.1"
   subscription_cost_management_exports = local.subscription_cost_management_exports
   depends_on                           = [module.subscriptions]
 }
 
 module "subscription_cost_management_views" {
-  source                             = "git::https://github.com/AeternaModules/azurerm_subscription_cost_management_view.git?ref=v5.0.0"
+  source                             = "git::https://github.com/AeternaModules/azurerm_subscription_cost_management_view.git?ref=v5.0.1"
   subscription_cost_management_views = local.subscription_cost_management_views
   depends_on                         = [module.subscriptions]
 }
 
 module "subscription_policy_assignments" {
-  source                          = "git::https://github.com/AeternaModules/azurerm_subscription_policy_assignment.git?ref=v5.0.0"
+  source                          = "git::https://github.com/AeternaModules/azurerm_subscription_policy_assignment.git?ref=v5.0.1"
   subscription_policy_assignments = local.subscription_policy_assignments
   depends_on                      = [module.subscriptions]
 }
 
 module "subscription_policy_exemptions" {
-  source                         = "git::https://github.com/AeternaModules/azurerm_subscription_policy_exemption.git?ref=v5.0.0"
+  source                         = "git::https://github.com/AeternaModules/azurerm_subscription_policy_exemption.git?ref=v5.0.1"
   subscription_policy_exemptions = local.subscription_policy_exemptions
   depends_on                     = [module.subscriptions, module.subscription_policy_assignments]
 }
 
 module "subscription_policy_remediations" {
-  source                           = "git::https://github.com/AeternaModules/azurerm_subscription_policy_remediation.git?ref=v5.0.0"
+  source                           = "git::https://github.com/AeternaModules/azurerm_subscription_policy_remediation.git?ref=v5.0.1"
   subscription_policy_remediations = local.subscription_policy_remediations
   depends_on                       = [module.subscriptions, module.subscription_policy_assignments]
 }
